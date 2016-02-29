cb65f13c41a2bd3ce4a58419c0c234f78390ff58
Edo Freriks <edofre@gmail.com>
2016-02-29 16:07:05 +0100

    Relations GII template fix

--- common/templates/yii2_enhanced/model/form.php
+++ common/templates/yii2_enhanced/model/form.php
@@ -9,7 +9,11 @@ echo $form->field($generator, 'ns');
 echo $form->field($generator, 'baseClass');
 echo $form->field($generator, 'db');
 echo $form->field($generator, 'useTablePrefix')->checkbox();
-echo $form->field($generator, 'generateRelations')->checkbox();
+echo $form->field($generator, 'generateRelations')->dropDownList([
+	\yii\gii\generators\model\Generator::RELATIONS_NONE => Yii::t('yii', 'No relations'),
+	\yii\gii\generators\model\Generator::RELATIONS_ALL => Yii::t('yii', 'All relations'),
+	\yii\gii\generators\model\Generator::RELATIONS_ALL_INVERSE => Yii::t('yii', 'All relations with inverse'),
+]);
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
 echo $form->field($generator, 'generateQuery')->checkbox();
 echo $form->field($generator, 'queryNs');
